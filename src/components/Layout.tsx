67d96956f92b0a1a3ad51495774c178ff4a3c3c6
aherreramilet <aherrera@dl.cl>
2025-08-15 20:31:39 -0300

    agregada analitycs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import CTA from '@/components/CTA';
 import WhatsAppButton from '@/components/WhatsAppButton';
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = () => {
   return (
@@ -11,6 +12,7 @@ const Layout = () => {
       {/* El <main> envuelve el contenido principal de cada página */}
       <main > {/* Padding para que el contenido no quede debajo del Header fijo */}
         <Outlet /> {/* Aquí es donde se renderizará el contenido de tu página (Index, ServiciosPage, etc.) */}
+        <Analytics />
       </main>
       <CTA /> {/* Opcional: si quieres el CTA en todas las páginas */}
       <Footer />
