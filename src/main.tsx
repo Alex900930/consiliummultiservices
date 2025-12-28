bb71df35d414b9feed3f65ec3d511e05b457523d
carolinbussines-blip <carol.inbussines@gmail.com>
2025-12-27 23:35:28 -0500

    Update main.tsx
    Add Meta Pixel tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,30 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/config'
+// 🔵 Meta Pixel
+if (typeof window !== 'undefined') {
+  !(function (f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod
+        ? n.callMethod.apply(n, arguments)
+        : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = true;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = true;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+  (window as any).fbq('init', '730161389707858');
+  (window as any).fbq('track', 'PageView');
+}
 
 createRoot(document.getElementById("root")!).render(
 
